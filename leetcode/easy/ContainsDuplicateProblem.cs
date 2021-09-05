6f59a6367097b3e773c61c29319bcb88fdd2adb3
mohitkhosla01 <mohit.khosla.01@gmail.com>
2021-09-05 16:27:42 +0530

    Added Contains Duplicate problem to Leetcode

---
+++ leetcode/easy/ContainsDuplicateProblem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algo_practice_csharp.leetcode.easy
+{
+    public class ContainsDuplicateProblem
+    {
+        private void Main(string[] args)
+        {
+            ContainsDuplicateProblem containsDuplicateProblem = new ContainsDuplicateProblem();
+
+            int[] nums1 = { 1, 2, 3, 1 };
+            Console.WriteLine(containsDuplicateProblem.ContainsDuplicate(nums1));
+
+            int[] nums2 = { 1, 2, 3, 4 };
+            Console.WriteLine(containsDuplicateProblem.ContainsDuplicate(nums2));
+
+            int[] nums3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            Console.WriteLine(containsDuplicateProblem.ContainsDuplicate(nums3));
+        }
+
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> numCount = new Dictionary<int, int>();
+
+            foreach(int num in nums)
+            {
+                if(numCount.ContainsKey(num))
+                {
+                    return true;
+                }
+                else
+                {
+                    numCount.Add(num, 1);
+                }
+            }
+
+            return false;
+        }
+    }
+}
