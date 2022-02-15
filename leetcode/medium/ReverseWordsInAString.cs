f6e6cf45091e3c92d212979cd57f23ca152ea763
mohitkhosla01 <mohit.khosla.01@gmail.com>
2022-02-15 20:42:12 +0530

    Added 'ReverseWordsInAString' to Leetcode

---
+++ leetcode/medium/ReverseWordsInAString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace algo_practice_csharp.leetcode.medium
+{
+    public class ReverseWordsInAString
+    {
+        private void Main(string[] args)
+        {
+            ReverseWordsInAString obj = new ReverseWordsInAString();
+
+            Console.WriteLine(obj.ReverseWords("the sky is blue"));
+            Console.WriteLine(obj.ReverseWords("  hello world  "));
+            Console.WriteLine(obj.ReverseWords("a good   example"));
+        }
+
+        public string ReverseWords(string s)
+        {
+            s = Regex.Replace(s, @"\s+", " ");
+            string[] sArr = s.Trim().Split(" ");
+            Array.Reverse(sArr);
+            return string.Join(" ", sArr);
+        }
+    }
+}
